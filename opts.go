c106344a7f04e74cdbd45f0593db0bf07ac0fdf6
HANAI Tohru <tohru@reedom.com>
2016-08-28 12:03:56 +0900

    Remove obsolete file and option

--- opts.go
+++ opts.go
@@ -1,9 +1,9 @@
 package logcat
 
 type opts struct {
-	Version bool   `short:"v" long:"version"`
-	TagPath string `short:"t" long:"tagfile" description:"Path to TAG definition file"`
-	Human   bool   `short:"H" long:"human" description:"Additionally print tag definition"`
+	Version bool `short:"v" long:"version"`
+	Human   bool `short:"H" long:"human" description:"Additionally print tag/value names"`
+	Indent  bool `short:"i" long:"indent" description:"Indent tag entries"`
 	Args    struct {
 		Paths []string
 	} `positional-args:"yes" required:"yes"`
